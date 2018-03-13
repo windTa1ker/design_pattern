30bb2102937c09cf72c5639f8f42e5f9ae1d64b0
windTa1ker <240902713@qq.com>
2018-03-13 11:40:11 +0800

    add design pattern adapter

---
+++ design_pattern/src/main/java/com/jingqi/practice/design_pattern/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package com.jingqi.practice.design_pattern.adapter;
+
+public interface AdvancedMediaPlayer {
+      public void playVlc(String fileName);
+      public void playMp4(String fileName);
+}
---
+++ design_pattern/src/main/java/com/jingqi/practice/design_pattern/adapter/MediaPlayer.java
@@ -0,0 +1,7 @@
+package com.jingqi.practice.design_pattern.adapter;
+/*
+ * 内容参考于http://www.runoob.com/design-pattern/adapter-pattern.html
+ */
+public interface MediaPlayer {
+      public void play(String audioType, String fileName);
+}
