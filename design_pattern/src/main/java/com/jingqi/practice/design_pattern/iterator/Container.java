e3c48a19854a94ff05288da9a9baaf95d96b8384
windTa1ker <240902713@qq.com>
2018-03-14 15:33:53 +0800

    add pattern iterator

---
+++ design_pattern/src/main/java/com/jingqi/practice/design_pattern/iterator/Container.java
@@ -0,0 +1,5 @@
+package com.jingqi.practice.design_pattern.iterator;
+
+public interface Container {
+      public Iterator getIterator();
+}
---
+++ design_pattern/src/main/java/com/jingqi/practice/design_pattern/iterator/Iterator.java
@@ -0,0 +1,6 @@
+package com.jingqi.practice.design_pattern.iterator;
+
+public interface Iterator {
+      public boolean hasNext();
+      public Object next();
+}
